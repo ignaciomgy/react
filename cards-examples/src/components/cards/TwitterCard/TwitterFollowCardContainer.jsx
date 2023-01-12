2144e373065f527966f5c89b3c9f28e7c2d2474e
Ignacio Migoya <ignacio.migoya@3sharecorp.com>
2023-01-12 14:31:03 -0300

    add limit 4 cards

--- cards-examples/src/components/cards/TwitterCard/TwitterFollowCardContainer.jsx
+++ cards-examples/src/components/cards/TwitterCard/TwitterFollowCardContainer.jsx
@@ -4,7 +4,7 @@ import { TwitterFollowCard } from './TwitterFollowCard';
 export function TwitterFollowCardContainer({ userName, name, isFollowing}) {
     const [posts, setPosts] = useState([]);
     useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/users')
+        fetch('https://jsonplaceholder.typicode.com/users?_limit=4')
             .then((response) => response.json())
             .then((data) => {
                 setPosts(data);
